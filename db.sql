1294b31592f0f938001a513215a708b4a4912b4f
jorge1b3 <jorge1b3@hotmail.es>
2024-10-25 21:06:39 -0500

    feat: replace id on authData and cart to use the forein key of profile. This has sence because they are all 1 to 1 relationships, and this desing forces to have a profile associed to auth and card and simplify the calls

--- db.sql
+++ db.sql
@@ -2,17 +2,17 @@
 
 create table Role (
     id SERIAL primary key,
-    name varchar(255)
+    name VARCHAR(255)
 );
 
 create table Permission (
     id SERIAL primary key,
-    name varchar(255)
+    name VARCHAR(255)
 );
 
 create table Role_Permission (
-    role_id int,
-    permission_id int,
+    role_id INTEGER,
+    permission_id INTEGER,
     primary key (role_id, permission_id),
     foreign key (role_id) references Role(id),
     foreign key (permission_id) references Permission(id)
@@ -20,22 +20,25 @@ create table Role_Permission (
 
 create table Profile (
     id SERIAL primary key,
-    name varchar(255),
-    phone varchar(255)
+    name VARCHAR(255),
+    phone VARCHAR(255)
 );
 
 create table AuthData (
-    id SERIAL primary key,
-    profile_id int,
-    username varchar(255),
-    password varchar(255),
-    enabled boolean default true,
-    foreign key (profile_id) references Profile(id)
+    id INTEGER primary key references Profile(id),
+    username VARCHAR(255),
+    password VARCHAR(255),
+    enabled boolean default true
+);
+
+create table Cart (
+    id INTEGER primary key references Profile(id),
+    enabled boolean default true
 );
 
 create table AuthData_Role (
-    authdata_id int,
-    role_id int,
+    authdata_id INTEGER,
+    role_id INTEGER,
     primary key (authdata_id, role_id),
     foreign key (authdata_id) references AuthData(id),
     foreign key (role_id) references Role(id)
@@ -44,20 +47,20 @@ create table AuthData_Role (
 
 create table Item (
     id SERIAL primary key,
-    name varchar(255),
-    price decimal(10, 2) default 0,
-    stock int default 0,
+    name VARCHAR(255),
+    price DECIMAL(10, 2) default 0,
+    stock INTEGER default 0,
     enabled boolean default true
 );
 
 create table Category (
     id SERIAL primary key,
-    name varchar(255)
+    name VARCHAR(255)
 );
 
 create table Item_Category (
-    category_id int,
-    item_id int,
+    category_id INTEGER,
+    item_id INTEGER,
     primary key (category_id, item_id),
     foreign key (category_id) references Category(id),
     foreign key (item_id) references Item(id)
@@ -65,36 +68,30 @@ create table Item_Category (
 
 create table Sale (
     id SERIAL primary key,
-    profile_id int,
+    profile_id INTEGER,
     date timestamp,
-    total decimal(10, 2),
-    status varchar(255),
-    comments varchar(255),
+    total DECIMAL(10, 2),
+    status VARCHAR(255),
+    comments VARCHAR(255),
     foreign key (profile_id) references Profile(id)
 );
 
 create table Sale_details (
-    sale_id int,
-    item_id int,
-    quantity int,
-    price decimal(10, 2),
+    sale_id INTEGER,
+    item_id INTEGER,
+    quantity INTEGER,
+    price DECIMAL(10, 2),
     primary key (sale_id, item_id),
     foreign key (sale_id) references Sale(id),
     foreign key (item_id) references Item(id)
 );
 
-create table Cart (
-    id SERIAL primary key,
-    profile_id int,
-    enabled boolean default true,
-    foreign key (profile_id) references Profile(id)
-);
 
 
 create table Cart_Item (
-    cart_id int,
-    item_id int,
-    quantity int,
+    cart_id INTEGER,
+    item_id INTEGER,
+    quantity INTEGER,
     primary key (cart_id, item_id),
     foreign key (cart_id) references Cart(id),
     foreign key (item_id) references Item(id)
